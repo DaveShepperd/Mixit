2ab3d92b778427324779e1ab66c54b933db697b9
Dave Shepperd <dshepperd7@gmail.com>
2025-10-28 20:42:50 -0700

    Fixed bug in 64 bit build with padding in img and dio output files.

--- putfile.c
+++ putfile.c
@@ -263,6 +263,7 @@ int putfile(char *fname, GPF *gpf)
 	uint32_t	rMax;
 	uint8_t	*buffer;
 	LogicalAddr	lo_range, hi_range, lo_ask, lo;
+	static const LogicalAddr MaxAddr = -2;
 	static	uint32_t		rec_count;
 	LogicalAddr inpOffset;
 	LogicalAddr outAddr;
@@ -516,7 +517,7 @@ int putfile(char *fname, GPF *gpf)
 			break;
 	}
 /* Only image and DIO files require padding */
-	if ( !(gpf->flags & GPF_M_NOPAD) && (gpf->rec_type == GPF_K_IMG || gpf->rec_type == GPF_K_DIO) && rBytes && hi_range < -2 && lo_ask <= hi_range )
+	if ( !(gpf->flags & GPF_M_NOPAD) && (gpf->rec_type == GPF_K_IMG || gpf->rec_type == GPF_K_DIO) && rBytes && hi_range < MaxAddr && lo_ask <= hi_range )
 	{
 		lo_ask = (lo_ask - lo_range) / gpf->bytes_per_word + gpf->out_add;
 		hi_range = (hi_range - lo_range) / gpf->bytes_per_word + gpf->out_add;
--- version.h
+++ version.h
@@ -18,6 +18,6 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_ (1)
 
-#define REVISION "V3.06"		/* MIXIT version number */
+#define REVISION "V3.07"		/* MIXIT version number */
 
 #endif	/* _VERSION_H_ */
